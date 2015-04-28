044ac9c6131f53ebfe50617719da12cf967bda3b
Ben D'Angelo <ben.dangelo2@gmail.com>
2015-04-27 20:49:54 -0400

    Added Kuftal Tunnel navmesh

--- enable_navmeshes.sql
+++ enable_navmeshes.sql
@@ -1,2 +1,2 @@
 -- Enable all available navmeshes in database
-update zone_settings set navmesh=1 where zoneid IN(160, 127, 103, 151, 126, 142, 101, 107, 106, 167, 100, 150);
+update zone_settings set navmesh=1 where zoneid IN(160, 127, 103, 151, 126, 142, 101, 107, 106, 167, 100, 150, 174);
